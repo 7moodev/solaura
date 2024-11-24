f1ea561e15723991b3834ad7c2556c8eb4d1d794
Trung Nguyen <trung.nguyen01@sap.com>
2024-11-24 03:36:36 +0100

    update

--- solaura_ui/src/app/page.tsx
+++ solaura_ui/src/app/page.tsx
@@ -3,9 +3,7 @@ import WalletAnalyzer from "@/components/wallet-analyzer";
 
 export default function Home() {
   return (
-<main
-  className="flex min-h-screen flex-col items-center justify-between text-gray-100 dark:text-gray-100 "
->
+    <main className="flex min-h-screen flex-col items-center justify-between text-gray-100 dark:text-gray-100 ">
       <Header />
       <WalletAnalyzer></WalletAnalyzer>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
@@ -22,12 +20,11 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Solana and the Metaplex programs from the developer hub.
+            Learn about Solaura.
           </p>
         </a>
-
         <a
-          href="https://github.com/metaplex-foundation"
+          href="https://github.com/7moodev/solaura"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -39,10 +36,9 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            The Metaplex Foundation&apos;s Github projects.
+            Solaura Github
           </p>
         </a>
-
         <a
           href="https://discord.com/invite/metaplex"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -59,7 +55,6 @@ export default function Home() {
             Come chat and find support in the Metaplex Discord server.
           </p>
         </a>
-
         <a
           href="https://x.com/metaplex"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
